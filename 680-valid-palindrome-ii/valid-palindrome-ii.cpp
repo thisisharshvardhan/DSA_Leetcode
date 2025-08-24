00f3542218f646200c3ece18dc7c0a9496e7786a
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2025-08-24 10:43:42 +0530

    Time: 7 ms (33.77%) | Memory: 26.2 MB (55.95%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool checkPal(string s, int i , int j){
+        while (i<=j){
+            if (s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while (i<=j){
+            if (s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else {
+                if (checkPal(s,i,j-1)||checkPal(s,i+1,j)){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+
+        }
+        return true;
+        
+    }
+};
