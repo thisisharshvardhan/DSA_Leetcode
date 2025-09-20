573e4930885bac9b8b822041415888746004be75
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2025-09-20 12:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (51.90%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -2,10 +2,9 @@
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        int m=score.size();
         
-        for (int i=0;i<m;i++){
-            for (int j=0;j<m-i-1;j++){
+        for (int i=0;i<score.size();i++){
+            for (int j=0;j<score.size()-i-1;j++){
                 if (score[j][k]<score[j+1][k]){
                     swap(score[j],score[j+1]);
                 }
