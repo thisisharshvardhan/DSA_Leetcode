8d64601f90165f89fbea1c29491e88da747c088c
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2025-11-10 23:24:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (75.90%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int ans=1;
+        int count =1;
+        for (int i=1;i<s.size();i++){
+            if (s[i]==s[i-1]) count +=1;
+            else count = 1;
+            ans = max(ans,count);
+        }
+        return ans;
+    }
+};
