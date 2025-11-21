4a432e7c2ea5c76f1497c4efb8d8ed5b2858f312
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2025-11-21 23:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (77.28%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int i=0;
+        while (i<s.size()){
+            if (i+k < s.size()){
+                reverse(s.begin()+i,s.begin()+i+k);
+            }
+            else {
+                reverse(s.begin()+i,s.end());
+            }
+            i=i+2*k;
+        }
+        return s;
+    }
+};
