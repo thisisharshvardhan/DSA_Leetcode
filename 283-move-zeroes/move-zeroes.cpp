ab73bb11b41c80d90b7e2a0092445a797f23747f
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2025-11-26 13:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (81.95%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,24 +1,12 @@
-#include <algorithm>
 class Solution {
 public:
-    int find(vector<int>& nums,int n){
-        for(int i=0;i<n;i++){
-            if (nums[i]==0){
-                return i;
-            }
-        }
-        return -1;
-    }
-
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        for (int i=0;i<n;i++){
-            if (find(nums,n)!= -1){
-                nums.erase(nums.begin() + find(nums,n));
-                nums.push_back(0);
+        int x=0;
+        for (int i=0;i<nums.size();i++){
+            if (nums[i]!=0){
+                swap(nums[i],nums[x]);
+                x++;
             }
         }
-        
-        
     }
 };
