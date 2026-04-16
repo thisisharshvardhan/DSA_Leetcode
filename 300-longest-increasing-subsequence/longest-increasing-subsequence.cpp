747d7dcf0381ee47614dc5bde8a0e0066219a3b1
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2026-04-16 18:49:40 +0530

    Time: 286 ms (26.67%) | Memory: 296.5 MB (17.44%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -12,6 +12,7 @@ public:
         return ans;
     } */
 
+/* 
         int solveMem(vector<int> &nums,int curr,int prev, vector<vector<int>>& dp){
         if (curr>=nums.size()) return 0;
         if (dp[curr][prev+1]!=-1) return dp[curr][prev+1];
@@ -24,19 +25,31 @@ public:
         int ans=max(inc,exc);
         dp[curr][prev+1]=ans;
         return ans;
-    }
-
-/*     int solveTab(vector<int> nums){
-
     } */
 
-
-    int lengthOfLIS(vector<int>& nums) {
+    int solveTab(vector<int> &nums){
         int prev=-1;
         int curr=0;
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        int ans=solveMem(nums,curr,prev,dp);
+        vector<vector<int>> dp (n+1,vector<int>(n+1,0));
+        for (int curr=n-1;curr>=0;curr--){
+            for (int prev=curr-1;prev>=-1;prev--){
+                    int inc=0;
+                    if (prev==-1 || nums[curr]>nums[prev]){
+                        inc = 1 + dp[curr+1][curr+1];
+                    }
+                    int exc = 0 + dp[curr+1][prev+1];
+                    dp[curr][prev+1]=max(inc,exc);
+                    
+            }
+        }
+        return dp[0][0];
+    }
+
+
+    int lengthOfLIS(vector<int>& nums) {
+        
+        int ans=solveTab(nums);
         return ans;
 
     }
