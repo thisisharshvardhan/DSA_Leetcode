0e848f9f9616266808e9273c53d13c67ffd17f90
Harsh Vardhan Singh <singhharshvardhan466@gmail.com>
2026-04-16 18:17:15 +0530

    Time: 721 ms (5.03%) | Memory: 296.6 MB (5.66%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+/*      int solveRec(vector<int> arr,int curr,int prev){
+        if (curr>=arr.size()) return 0;
+
+        int inc=0;
+        if (prev==-1 || arr[curr]>arr[prev]){
+            inc = 1 + solveRec(arr, curr+1,curr);
+        }
+        int exc = 0 + solveRec(arr,curr+1,prev);
+        int ans=max(inc,exc);
+        return ans;
+    } */
+
+        int solveMem(vector<int> &nums,int curr,int prev, vector<vector<int>>& dp){
+        if (curr>=nums.size()) return 0;
+        if (dp[curr][prev+1]!=-1) return dp[curr][prev+1];
+
+        int inc=0;
+        if (prev==-1 || nums[curr]>nums[prev]){
+            inc = 1 + solveMem(nums, curr+1,curr,dp);
+        }
+        int exc = 0 + solveMem(nums,curr+1,prev,dp);
+        int ans=max(inc,exc);
+        dp[curr][prev+1]=ans;
+        return ans;
+    }
+
+/*     int solveTab(vector<int> nums){
+
+    } */
+
+
+    int lengthOfLIS(vector<int>& nums) {
+        int prev=-1;
+        int curr=0;
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        int ans=solveMem(nums,curr,prev,dp);
+        return ans;
+
+    }
+};
